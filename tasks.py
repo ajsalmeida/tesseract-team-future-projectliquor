ab7c52215034a6753d20c0af863bf9273d8de8f1
andrerodrig <andrelmarques11@gmail.com>
2021-11-20 11:23:30 -0300

    Cria nova configuração para o backend de building do projeto

--- tasks.py
+++ tasks.py
@@ -4,8 +4,8 @@ from invoke import task
 from pathlib import Path
 from zipfile import ZipFile
 
-
-data_dir = Path("data") / "raw"
+root_path = Path("./")
+data_dir = root_path / Path("data") / "raw"
 
 @task
 def get_dataset(c):
@@ -14,10 +14,23 @@ def get_dataset(c):
     """
     if "Iowa_Liquor_Sales.csv" not in os.listdir(data_dir):
         c.run(
-            f"poetry run kaggle datasets download -d residentmario/iowa-liquor-sales -p {data_dir}"
+            f"kaggle datasets download -d residentmario/iowa-liquor-sales -p {data_dir}"
         )
         print("Extracting data...")
         with ZipFile(data_dir / "iowa-liquor-sales.zip", "r") as dataset_obj:
             dataset_obj.extractall(path=data_dir)
         c.run(f"rm -rf {data_dir / 'iowa-liquor-sales.zip'}")
     print("Done!")
+
+
+@task
+def install(c):
+    """Project installation.
+
+    Task to automate the project reinstallation. It creates the virtualenv and install the package.
+    """
+    if build_path := "dist" in os.listdir(root_path):
+        c.run(f"rm -rf {build_path}")
+    c.run("python -m build")
+    c.run("pip install -e .[dev]")
+
